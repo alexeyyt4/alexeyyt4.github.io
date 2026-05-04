ff8f383831db1d92edb2677b43fd797381e27b69
Олексій Дегтяренко <122479408+alexeyyt4@users.noreply.github.com>
2026-05-04 12:38:35 +0300

    Update server title and background image in config

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ var Config = {};
  * What should the text in the center of the screen be?
  * if empty it will fill in your Server Name
  */
-Config.title = "IRS | SCP MilitaryRP";
+Config.title = "IRS | HL: HECURP";
 
 /**
  * Enable map text in the top left corner of the screen?
@@ -28,14 +28,13 @@ Config.enableAnnouncements = true;
  */
 Config.announceMessages = [
   "Бажаю вам гарного РП процесу",
-  "Охороняти. Утримувати. Захищати.",
-  "Цікавий факт: ГОК не тільки знищує аномалії. Вони їх ще досліджують.",
-  "Цікавий факт: А... А відділ антимеметики в нас існує?",
-  "Цікавий факт: ДЗ за звілнення та захист розумних SCP-об'єктів, а не за усіх дцп аномалій",
-  "Цікавий факт: Сірєногаловий не є SCP-об'єктом",
+  "Забудьте про Фрімена!",
+  "Цікавий факт: В інтернеті доси тривають сварки на рахунок роду військ HECU",
+  "Цікавий факт: Цей сервер створено з нуля, не використовуючи злиті збірки",
+  "Цікавий факт: Лише ви вирішуєте судьбу вашого персонажа",
+  "Цікавий факт: Проект існує понад рік!",
   "Тут не раді кацапам.",
-  "Цікавий факт: [ДАНІ ВИДАЛЕНІ]",
-  "Українська філія фонду дуже цікава!"
+  "Цікавий факт: [ДАНІ ВИДАЛЕНІ]"
 ];
 
 /**
@@ -48,7 +47,7 @@ Config.announcementLength = 5000;
  * Image Filename
  * DROP IMAGE IN "images" FOLDER
  */
-Config.backgroundImage = "mtf.png";
+Config.backgroundImage = "hecu.jpg";
 
 /**
  * Enable debug messages?
